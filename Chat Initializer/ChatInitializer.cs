095b43bc9f4659b3ab50fc76acc68bc3608cbe82
Felladrin <felladrin@gmail.com>
2014-03-22 16:34:29 -0300

    Added Chat Initializer

---
+++ Chat Initializer/ChatInitializer.cs
@@ -0,0 +1,130 @@
+﻿//   ___|========================|___
+//   \  |  Written by Felladrin  |  /   This script was released on RunUO Community under the GPL licensing terms.
+//    > |      August 2013       | <
+//   /__|========================|__\   [Chat Initializer] - Current version: 1.0 (August 12, 2013)
+//
+//   To avoid the message "Chat is not currently supported" on login, remove the file Scripts/Engines/Chat/Chatold.cs
+
+using Server.Commands;
+using Server.Accounting;
+
+namespace Server.Engines.Chat
+{
+    class ChatInitializer
+    {
+        public static void Initialize()
+        {
+            CommandSystem.Register("Chat", AccessLevel.Player, new CommandEventHandler(OpenChat_OnCommand));
+            EventSink.Login += new LoginEventHandler(EventSink_Login);
+            EventSink.Logout += new LogoutEventHandler(EventSink_Logout);
+            EventSink.Disconnected += new DisconnectedEventHandler(EventSink_Disconnected);
+            EventSink.ChatRequest += new ChatRequestEventHandler(EventSink_ChatRequest);
+        }
+
+        [Usage("Chat")]
+        [Description("Enables or Disables the Chat.")]
+        private static void OpenChat_OnCommand(CommandEventArgs args)
+        {
+            Mobile player = args.Mobile;
+
+            if (player.Account == null)
+            {
+                player.SendMessage(38, "You have no account! Report immediately to the staff!");
+                return;
+            }
+
+            Account account = player.Account as Account;
+
+            if (account.GetTag("ChatInitializer") != "OK")
+            {
+                account.SetTag("ChatInitializer", "OK");
+                player.SendMessage(68, "Chat: ENABLED");
+                DisplayChatTo(player);
+            }
+            else
+            {
+                account.SetTag("ChatInitializer", "NO");
+                player.SendMessage(38, "Chat: DISABLED");
+                HideChatFrom(player);
+            }
+        }
+
+        private static void EventSink_Login(LoginEventArgs args)
+        {
+            Mobile player = args.Mobile;
+
+            if (player.Account == null)
+                return;
+
+            Account account = player.Account as Account;
+
+            DefineChatName(player);
+
+            if (account.GetTag("ChatInitializer") == "OK")
+            {
+                DisplayChatTo(player);
+            }
+        }
+
+        private static void EventSink_Logout(LogoutEventArgs args)
+        {
+            Mobile player = args.Mobile;
+            HideChatFrom(player);
+        }
+
+        private static void EventSink_Disconnected(DisconnectedEventArgs args)
+        {
+            Mobile player = args.Mobile;
+            HideChatFrom(player);
+        }
+
+        private static void EventSink_ChatRequest(ChatRequestEventArgs e)
+        {
+            Mobile player = e.Mobile;
+
+            if (player.Account == null)
+                return;
+
+            Account account = player.Account as Account;
+
+            if (account.GetTag("ChatInitializer") != "OK")
+            {
+                account.SetTag("ChatInitializer", "OK");
+                player.SendMessage(68, "Chat: ENABLED");
+                DisplayChatTo(player);
+            }
+        }
+
+        private static void DisplayChatTo(Mobile player)
+        {
+            if (player.Account == null)
+                return;
+
+            Account account = player.Account as Account;
+            string accountChatName = account.GetTag("ChatName");
+            ChatSystem.SendCommandTo(player, ChatCommand.OpenChatWindow, accountChatName);
+            ChatUser.AddChatUser(player);
+        }
+
+        private static void HideChatFrom(Mobile player)
+        {
+            if (player.Account == null)
+                return;
+
+            Account account = player.Account as Account;
+            ChatSystem.SendCommandTo(player, ChatCommand.CloseChatWindow);
+            ChatUser user = ChatUser.GetChatUser(player);
+            ChatUser.RemoveChatUser(user);
+        }
+
+        private static void DefineChatName(Mobile player)
+        {
+            if (player.Account == null)
+                return;
+
+            Account account = player.Account as Account;
+            string accountChatName = (player.RawName).Replace(" ", "");
+            account.SetTag("ChatName", accountChatName);
+        }
+    }
+}
---
+++ Chat Initializer/README.md
@@ -0,0 +1,21 @@
+## Chat Initializer ##
+
+For those who are still using UO Client 5.x or 6.x and would like to make use of the ingame chat.
+
+This script will auto-open the chat client and add the player, based on his current character name.
+
+All you need to do is to drop this script somewhere in your Scripts folder.
+
+Players can use the command `[Chat` to enable or disable the ChatInitializer.
+
+As I'm not using it anymore, I don't have a screenshot of it, but here is a screenshot [posted by another member][1] of community that shows the old chat box:
+
+![\[IMG\]][2]
+
+Note that for it to work you have to make sure the Chat Engine is enabled in the file _Scripts/Engines/Chat/Chat.cs_
+
+It has only been tested with clients 5.x and 6.x. It may not work properly on older or newer UO clients.
+
+   [1]: http://www.runuo.com/community/threads/ultima-online-chat-box.98003/#post-816969
+   [2]: http://i.imgur.com/qvqudtR.gif
+  
