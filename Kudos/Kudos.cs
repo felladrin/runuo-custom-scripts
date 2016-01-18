f3287687517e80c7346f09a42a7efefb71f71e17
Victor Nogueira <felladrin@gmail.com>
2016-01-18 15:43:47 -0300

    Updated Kudos.

---
+++ Kudos/Kudos.cs
@@ -0,0 +1,111 @@
+﻿// Kudos v1.4.0
+// Author: Felladrin
+// Created at 2013-07-23
+// Updated at 2016-01-18
+
+using System;
+using Server;
+using Server.Mobiles;
+using Server.Network;
+
+namespace Felladrin.Items
+{
+    public class Kudos : Item
+    {
+        public static class Config
+        {
+            public static bool Enabled = true;                              // Is the kudos auto distribution enabled?
+            public static int MinutesOnline = 1;                            // Every X minutes we give kudos to all players online.
+            public static bool DropOnBank = true;                           // Should we place the kudos on character's bankbox (true) or backpack (false)?
+            public static AccessLevel MaxAccessLevel = AccessLevel.Player;  // Any character with this access and below receives kudos.
+        }
+
+        public static void Initialize()
+        {
+            if (Config.Enabled)
+                new KudosAutoDistributionTimer().Start();
+        }
+
+        [Constructable]
+        public Kudos(int amount)
+        {
+            Name = "Kudos";
+            ItemID = 0xF11;
+            Hue = 0x47E;
+            Stackable = true;
+            Amount = amount;
+            Weight = 0;
+        }
+
+        [Constructable]
+        public Kudos() : this(1) { }
+
+        public override void GetProperties(ObjectPropertyList list)
+        {
+            base.GetProperties(list);
+            list.Add("Exchangeable Game Time Reward");
+        }
+
+        public override bool DisplayWeight { get { return false; } }
+
+        public Kudos(Serial serial) : base(serial) { }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+            writer.Write(0);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+            reader.ReadInt();
+        }
+
+        class KudosAutoDistributionTimer : Timer
+        {
+            public KudosAutoDistributionTimer() : base(TimeSpan.Zero, TimeSpan.FromMinutes(Config.MinutesOnline))
+            {
+                Priority = TimerPriority.OneMinute;
+            }
+
+            protected override void OnTick()
+            {
+                foreach (NetState state in NetState.Instances)
+                {
+                    PlayerMobile m = state.Mobile as PlayerMobile;
+
+                    if (m != null && m.AccessLevel <= Config.MaxAccessLevel)
+                    {
+                        if (Config.DropOnBank && m.BankBox != null)
+                        {
+                            Item kudos = m.BankBox.FindItemByType(typeof(Kudos));
+
+                            if (kudos != null)
+                            {
+                                kudos.Amount++;
+                            }
+                            else
+                            {
+                                m.BankBox.DropItem(new Kudos());
+                            }
+                        }
+                        else if (m.Backpack != null)
+                        {
+                            Item kudos = m.Backpack.FindItemByType(typeof(Kudos));
+
+                            if (kudos != null)
+                            {
+                                kudos.Amount++;
+                            }
+                            else
+                            {
+                                m.Backpack.DropItem(new Kudos());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Kudos/README.md
@@ -0,0 +1,36 @@
+# Kudos - Exchangeable Game Time Reward
+
+This is an automatic system that gives an item (Kudos) for all players online every minute (this time is configurable). They can exchange their Kudos for rewards using some other scripts from the RunUO community, like the [Complete Customizable Vendor][1].
+
+## Installation
+
+Just drop this script somewhere in your Scripts folder.
+
+## Configuration
+
+You can configure this system at the top of the script.
+
+	bool Enabled = true;                              // Is the kudos auto distribution enabled?
+	int MinutesOnline = 1;                            // Every X minutes we give kudos to all players online.
+	bool DropOnBank = true;                           // Should we place the kudos on character's bankbox (true) or backpack (false)?
+	AccessLevel MaxAccessLevel = AccessLevel.Player;  // Any character with this access and below receives kudos.
+
+## Screenshots
+
+![\[IMG\]][2]<br/>
+_Exchanging kudos at the customizable vendor_
+
+![\[IMG\]][3]<br/>
+_By default, players receive their kudos on their bankbox_
+
+## Exchange price suggestion ##
+
+- **Tiny Items:** From 10 to 1k kudos
+- **Small Items:** From 1K to 2K kudos
+- **Medium Items:** From 2K to 3K kudos
+- **Big Items:** From 3K to 4K kudos
+- **Huge Items:** From 4K to 5K kudos
+
+   [1]: http://www.runuo.com/community/threads/runu-o-2-0-rc1-rc2-the-complete-customizable-vendor.91051/
+   [2]: http://i.imgur.com/7k2ybTv.png
+   [3]: http://i.imgur.com/L8o4YS3.png
