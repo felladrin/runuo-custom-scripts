85d293c4d97a5c86b45adcb36024714765f0e472
Victor Nogueira <felladrin@gmail.com>
2016-01-21 16:54:42 -0300

    Added TurnBackToDeed Command.

---
+++ TurnBackToDeed Command/README.md
@@ -0,0 +1,7 @@
+# TurnBackToDeed Command
+
+Player command used to turn back to deed a furniture from their house.
+
+This work the same as equipping an axe and chopping an addon inside the house.
+
+To install, just drop this script anywhere inside your Scripts folder.
---
+++ TurnBackToDeed Command/TurnBackToDeed.cs
@@ -0,0 +1,48 @@
+// Time Command v1.1.0
+// Author: Felladrin
+// Started: 2008-01-13
+// Updated: 2016-01-21
+
+using Server;
+using Server.Commands;
+using Server.Targeting;
+
+namespace Felladrin.Commands
+{
+    public static class TurnBackToDeed
+    {
+        public static void Initialize()
+        {
+            CommandSystem.Register("TurnBackToDeed", AccessLevel.Player, new CommandEventHandler(OnCommand));
+        }
+
+        [Usage("TurnBackToDeed")]
+        [Description("Turns back to deed a furniture from your house.")]
+        static void OnCommand(CommandEventArgs e)
+        {
+            e.Mobile.Target = new DismantleTarget();
+            e.Mobile.SendMessage("Select the furnitures you want to turn back to deed. Press ESC when you finish.");
+        }
+
+        class DismantleTarget : Target
+        {
+            public DismantleTarget() : base(2, false, TargetFlags.None) { }
+
+            protected override void OnTarget(Mobile from, object targeted)
+            {
+                var chopable = targeted as IChopable;
+
+                if (chopable != null)
+                {
+                    chopable.OnChop(from);
+                }
+                else
+                {
+                    from.SendMessage("That object can't be turned back to deed.");
+                }
+
+                from.Target = new DismantleTarget();
+            }
+        }
+    }
+}
