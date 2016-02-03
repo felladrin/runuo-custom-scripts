ec8c5a79e313b14a246bf4bd83399869ae0d0e2c
Victor Nogueira <felladrin@gmail.com>
2016-02-03 02:34:52 -0300

    Improved Clone Character On Logout Script to clone all offline playermobiles on the first run of the script and to correctly clone the mount and skills of the character.

--- Clone Character On Logout/CloneCharacterOnLogout.cs
+++ Clone Character On Logout/CloneCharacterOnLogout.cs
@@ -1,7 +1,7 @@
-﻿// Clone Character On Logout v1.0.0
+﻿// Clone Character On Logout v1.1.0
 // Author: Felladrin
 // Started: 2016-01-25
-// Updated: 2016-01-25
+// Updated: 2016-02-02
 
 using System.Collections.Generic;
 using System.Reflection;
@@ -25,30 +25,50 @@ namespace Felladrin.Automations
             {
                 EventSink.Logout += OnLogout;
                 EventSink.Login += OnLogin;
+                CheckFirstRun();
             }
         }
 
         static void OnLogout(LogoutEventArgs e)
         {
-            if (e.Mobile.AccessLevel != AccessLevel.Player)
-                return;
-
-            var characterOriginal = e.Mobile;
-            var characterClone = new CharacterClone(characterOriginal);
-
-            foreach (var itemOriginal in characterOriginal.Items)
-                if (itemOriginal.Parent == characterOriginal)
-                    characterClone.AddItem(new ItemClone(itemOriginal));
+            if (e.Mobile.AccessLevel == AccessLevel.Player)
+                CreateCloneOf(e.Mobile);
         }
 
         static void OnLogin(LoginEventArgs e)
         {
-            if (e.Mobile.AccessLevel != AccessLevel.Player)
-                return;
+            if (e.Mobile.AccessLevel == AccessLevel.Player)
+                DeleteClonesOf(e.Mobile);
+        }
 
-            foreach (var m in new List<Mobile>(World.Mobiles.Values))
-                if (m is CharacterClone && ((CharacterClone)m).Original == e.Mobile)
-                    m.Delete();
+        static void CreateCloneOf(Mobile m)
+        {
+            var characterClone = new CharacterClone(m);
+
+            foreach (var itemOriginal in m.Items)
+                if (itemOriginal.Parent == m && itemOriginal.Layer != Layer.Mount)
+                    characterClone.AddItem(new ItemClone(itemOriginal));
+
+            if (m.Mounted)
+                new MountClone((BaseMount)m.Mount).Rider = characterClone;
+        }
+
+        static void DeleteClonesOf(Mobile m)
+        {
+            foreach (var mobile in new List<Mobile>(World.Mobiles.Values))
+                if (mobile is CharacterClone && ((CharacterClone)mobile).Original == m)
+                    mobile.Delete();
+        }
+
+        static void CheckFirstRun()
+        {
+            foreach (var mobile in World.Mobiles.Values)
+                if (mobile is CharacterClone)
+                    return;
+
+            foreach (var mobile in new List<Mobile>(World.Mobiles.Values))
+                if (mobile is PlayerMobile && mobile.Alive && mobile.AccessLevel == AccessLevel.Player)
+                    CreateCloneOf(mobile);
         }
 
         public class CharacterClone : BaseCreature
@@ -64,18 +84,21 @@ namespace Felladrin.Automations
                     if (property.CanRead && property.CanWrite)
                         property.SetValue(this, property.GetValue(Original, null), null);
 
+                for (int i = 0, l = Original.Skills.Length; i < l; ++i)
+                    Skills[i].Base = Original.Skills[i].Base;
+
                 Player = false;
+
+                if (Map == Map.Internal)
+                    Map = LogoutMap;
             }
 
             public override void GetProperties(ObjectPropertyList list)
             {
                 if (Original != null)
-                {
                     Original.GetProperties(list);
-                    return;
-                }
-
-                base.GetProperties(list);
+                else
+                    base.GetProperties(list);
             }
 
             public override bool IsInvulnerable{ get{ return true; } }
@@ -123,12 +146,9 @@ namespace Felladrin.Automations
             public override void GetProperties(ObjectPropertyList list)
             {
                 if (Original != null)
-                {
                     Original.GetProperties(list);
-                    return;
-                }
-
-                base.GetProperties(list);
+                else
+                    base.GetProperties(list);
             }
 
             public ItemClone(Serial serial) : base(serial) { }
@@ -148,6 +168,30 @@ namespace Felladrin.Automations
 
                 if (Original == null)
                     Delete();
+            }
+        }
+
+        public class MountClone : BaseMount
+        {
+            public MountClone(BaseMount original) : base(original.Name,  original.BodyValue, original.ItemID, original.AI, original.FightMode, original.RangePerception, original.RangeFight, original.ActiveSpeed, original.PassiveSpeed)
+            {
+                foreach (var property in (typeof(BaseMount)).GetProperties())
+                    if (property.CanRead && property.CanWrite && property.Name != "Rider")
+                        property.SetValue(this, property.GetValue(original, null), null);
+            }
+
+            public MountClone(Serial serial) : base(serial) { }
+
+            public override void Serialize(GenericWriter writer)
+            {
+                base.Serialize(writer);
+                writer.Write(0);
+            }
+
+            public override void Deserialize(GenericReader reader)
+            {
+                base.Deserialize(reader);
+                reader.ReadInt();
             }
         }
     }
